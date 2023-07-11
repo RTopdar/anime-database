e295a930ad358eab34835441bb3e854e288c1a27
Rounak Topdar <rounak2002topdar@gmail.com>
2023-07-11 21:18:29 +0530

    fixed my code and changed functionality

--- src/app/MyComponents/home/home.component.ts
+++ src/app/MyComponents/home/home.component.ts
@@ -13,7 +13,8 @@ export class HomeComponent implements OnInit{
   }
   allAnime: any;
   trendingAnime: any;
-  private url: string = `https://api.jikan.moe/v4/anime?q=`
+  private url: string = `https://api.jikan.moe/v4/anime?q=`;
+  private malUrl: string = `https://myanimelist.net/anime/`;
   searchTerm: string = "";
   searchAnime(e: Event){
     e.preventDefault();
@@ -40,68 +41,10 @@ export class HomeComponent implements OnInit{
       });
     });
   }
+  
+  redirect(id:number){
+    window.open(`${this.malUrl}${id}`,"_blank");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   trendingAnime: any;
-//   private url: string = `https://api.jikan.moe/v4/anime?q=`;
-// <<<<<<< HEAD
-//   e: Event | any;
-// =======
-// >>>>>>> parent of 67fcb65 (Added search functionality)
-//   constructor(private http: HttpClient) {
-
-//   }
-//   ngOnInit(): void {
-//     this.getPopularAnime();
-// <<<<<<< HEAD
-//     this.searchAnime(this.e);
-//   }
-//   searchAnime(event: any){
-//     event.preventDefault();
-//     fetch(`${this.url}${this.searchTerm}`).then((Response) => {
-//       Response.json().then((Result) => {
-//         this.allAnime = Result.data;
-//         // if(this.searchTerm!=='')
-//         {
-//           console.log(this.allAnime);
-//         }
-//       });
-//     });
-// =======
-// >>>>>>> parent of 67fcb65 (Added search functionality)
-//   }
-//   getPopularAnime(){
-//     fetch(`${this.url}`).then((Response) => {
-//       // console.log(Response);
-//       Response.json().then((Res) => {
-//         // const res = Res.data;
-//         this.trendingAnime = Res.data;
-//         this.trendingAnime.splice(0,3);
-//         this.trendingAnime.sort((a: any,b: any)=>a.rank-b.rank);
-//         console.log(this.trendingAnime);
-//         // res.map((item: any, index: number) => {
-        
-//         // });
-//       });
-//     });
-
-  //   this.http
-  //     .get(this.url)
-  //     .subscribe((anime) => {
-  //       this.trendingAnime = anime; 
-  // })
+  }
 }
+
